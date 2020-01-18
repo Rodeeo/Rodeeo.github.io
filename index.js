ee425c96442143526358ddcb6097f2703f478433
Rodeeo <therodee@gmail.com>
2020-01-18 16:58:45 -0700

    lastmod

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 var lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = lastModified;
-
-const options = {year: 'numeric'};
-document.getElementById('currentYear').textContent = new Date().toLocaleDateString('en-US', options);
