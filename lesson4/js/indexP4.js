7bd5cbd6a95f061288669e9ee3e660303481dc44
Rodeeo <therodee@gmail.com>
2020-02-06 16:02:21 -0700

    P4 P5

--- lesson4/js/indexP4.js
+++ lesson4/js/indexP4.js
@@ -1,4 +1,4 @@
-const dayz = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
+const dayz = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const monthz = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 let today = new Date;
