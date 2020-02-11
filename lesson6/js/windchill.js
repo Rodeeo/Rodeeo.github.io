0ad77c0e531bb9d376608e71717c8cc648485632
Rodeeo <therodee@gmail.com>
2020-02-11 12:21:27 -0700

    Wind chill function working!

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,11 +1,18 @@
 function showWindChill() {
-    let temp = 45;
-    let speed = 10;
-    let wind = windChill(temp, speed);
-let print = (document.getElementById("outputDiv").innerHTML = "Wind Chill: " + wind.toFixed(2));
+  const temp = 50;
+  const speed = 10;
+
+  if (temp <= 50 && speed > 3) {
+    const wind = windChill(temp, speed);
+    document.getElementById("outputDiv").innerHTML = "Wind Chill: " + wind.toFixed(2);
   }
-  function windChill(tempf, speed) {
-    let a = Math.pow(speed, 0.16);
-    let calculate = 35.74 + 0.6215 * tempf - 35.75 * a + 0.4275 * tempf * a;
-    return calculate;
-  }
+  else { 
+    document.getElementById("outputDiv").innerHTML = "Wind Chill: N/A";
+  } 
+}
+function windChill(tempf, speed) {
+  const a = Math.pow(speed, 0.16);
+  const calculate = 35.74 + 0.6215 * tempf - 35.75 * a + 0.4275 * tempf * a;
+  return calculate;
+}
+ 
