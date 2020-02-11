c6fc21969f6cec79c7e7c9eed2f0bc6119acb543
Rodeeo <therodee@gmail.com>
2020-02-10 20:08:03 -0700

    lesson 6

---
+++ lesson6/js/friday.js
@@ -0,0 +1,7 @@
+
+function checkDate() {
+    var date = new Date();
+    console.log(date.getDay());
+    if(date.getDay() === 5) {document.getElementById("friday").innerHTML = "Saturday = Preston Pancakes in the Park! <br> 9:00 a.m. Saturday at the city park pavilion.";
+    } 
+}
