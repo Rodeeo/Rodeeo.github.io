6673993f6718962a4437dd13b2e2151722c7cbf7
Rodeeo <therodee@gmail.com>
2020-02-20 19:07:58 -0700

    lazyload finish

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,9 +1,8 @@
-// get all imgs with data-src attribute
 const imagesToLoad = document.querySelectorAll('img[data-src]');
 
-// optional parameters being set for the IntersectionalObserver
+
 const imgOptions = {
-    threshold: 0,
+    threshold: 1,
     rootMargin: "0px 0px 50px 0px"
 };
 
@@ -14,22 +13,19 @@ const loadImages = (image) => {
     };
 };
 
-// first check to see if Intersection Observer is supported
 if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
+    const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
             if (item.isIntersecting) {
                 loadImages(item.target);
-                imgObserver.unobserve(item.target);
+                observer.unobserve(item.target);
             }
         });
-    }, imgOptions);
-
-    // Loop through each img an check status and load if necessary
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
     });
-} else { // just load ALL images if not supported
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
     });
