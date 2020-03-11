060408e5ad14da43c3b45434d1054ec1a9c2525c
Rodeeo <therodee@gmail.com>
2020-03-10 21:48:27 -0600

    favicon

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,15 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=da65eb66c969087514d2a228682d94b6";
+fetch(apiURL)                                                                                   
+
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+    const desc = jsObject.weather[0].description;  
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);  
+  });
+
