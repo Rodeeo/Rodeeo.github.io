10352d07604737dea5c184dd132950995d0dfcb5
Rodeeo <therodee@gmail.com>
2020-03-11 19:19:28 -0600

    api fix

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -13,16 +13,5 @@ fetch(apiURL)
     document.getElementById('icon').setAttribute('alt', desc);  
   });
 
-  const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=da65eb66c969087514d2a228682d94b";
-  fetch(apiURL)
-      .then((response) => response.json())
-      .then((jsObject) => {
-  
-      document.getElementById('currently').textContent = jsObject.weather.description;
-      document.getElementById('temperature').textContent = jsObject.main.temp;
-      document.getElementById('humidty').textContent = jsObject.main.humidity;
-      document.getElementById('windspeed').textContent = jsObject.wind.speed;
-  
-      getWindChill();
-    });
+
   
