3604763f403792c8134aa0c83290b4c279726740
Rodeeo <therodee@gmail.com>
2020-03-15 14:25:03 -0600

    section change

---
+++ lesson11/js/countdown.js
@@ -0,0 +1,16 @@
+var countDownDate = new Date("April 8, 2020 11:59:59").getTime();
+  var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML = "End of Semester: " + days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "ON TO THE NEXT ONE!!!";
+  }
+}, 1000);
