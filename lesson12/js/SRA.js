f642a08b78592a04803e5662652f00851e305499
Rodeeo <therodee@gmail.com>
2020-03-19 10:45:02 -0600

    SRA

---
+++ lesson12/js/SRA.js
@@ -0,0 +1,26 @@
+// WEBFONT
+WebFont.load({ google: { families: ['Fredoka One', 'Fugaz One', 'Gugi', 'Squada One'] } }); 
+
+// MENU BUTTON
+const hambutton = document.querySelector(".boom");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu( ) {
+    document.querySelector(".dynamite").classList.toggle("explode");
+}
+
+// FOOTER INFO
+const theDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let today = new Date;
+let day = today.getDate()
+let dayName = theDays[today.getDay()];
+let month = theMonths [today.getMonth()];
+let year = today.getFullYear();
+
+document.getElementById("footinfo").textContent = `${dayName}, ${day} ${month} ${year}`
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
