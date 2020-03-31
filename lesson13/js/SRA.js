1be9782154c423d2b8ee7874967d53970f76a75c
Rodeeo <therodee@gmail.com>
2020-03-30 21:29:20 -0600

    image bending

--- lesson13/js/SRA.js
+++ lesson13/js/SRA.js
@@ -31,8 +31,8 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
 
-    document.getElementById('temperature').textContent = jsObject.main.temp.toFixed(0) + "˚ " + jsObject.weather[0].description; 
-    document.getElementById('high').textContent = jsObject.main.temp_max.toFixed(0) + "˚";
+    document.getElementById('temperature').textContent = jsObject.main.temp.toFixed(0) + "\xB0 " + jsObject.weather[0].description; 
+    document.getElementById('high').textContent = jsObject.main.temp_max.toFixed(0) + "\xB0";
     document.getElementById('humidity').textContent = jsObject.main.humidity + "%";
     document.getElementById('windspeed').textContent = jsObject.wind.speed.toFixed(0) + "mph";
 
@@ -41,7 +41,7 @@ let windspeed = parseInt(document.getElementById("windspeed").innerText);
 
 if (temp <= 50  && windspeed >= 3) {
     let windchilly = 35.74 + (0.6215*temp) - (35.75*(windspeed**0.16)) + (0.4275*temp*(windspeed**0.16));
-    document.getElementById("windchill").innerText = windchilly.toFixed(0)+ "˚";
+    document.getElementById("windchill").innerText = windchilly.toFixed(0)+ "\xB0";
     } 
 else {
    document.getElementById("windchill").innerText = " N/A" 
