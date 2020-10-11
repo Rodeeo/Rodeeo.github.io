e76867d27ce67a293397e0c0dd6b34500fb628fc
Rodeeo <49972420+Rodeeo@users.noreply.github.com>
2020-10-10 20:29:33 -0600

    hero

--- WDD 330/Week 4/Homework/hero.js
+++ WDD 330/Week 4/Homework/hero.js
@@ -1,10 +1,14 @@
+
+
 const form = document.forms.hero;
+
 form.addEventListener('submit', makeHero, false);
 
 function makeHero(event) {
     event.preventDefault(); // prevent the form from being submitted
     const hero = {}; // create an empty object
     hero.name = form.heroName.value; // create a name
+   
     hero.realName = form.realName.value;
     hero.category = form.category.value;
     hero.age = form.age.value;
@@ -18,6 +22,30 @@ function makeHero(event) {
     hero.origin = form.origin.value;   
       
     alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
-  
-    return hero;
-}
+ 
+return hero;
+    }
+
+    
+    const label = form.querySelector('label');
+    const error = document.createElement('div');
+    error.classList.add('error');
+    error.textContent = '! Your name is not allowed to start with X.';
+    label.append(error);
+    function validateInline() {
+        const heroName = this.value.toUpperCase();
+        if(heroName.startsWith('X')){
+        error.style.display = 'block';
+        } else {
+        error.style.display = 'none';
+        }
+    }
+    function disableSubmit(event) {
+        if(event.target.value === ''){
+            document.getElementById('submit').disabled = true;
+        } else {
+            document.getElementById('submit').disabled = false;
+        }
+    }
+    form.heroName.addEventListener('keyup',disableSubmit,false);
+   
