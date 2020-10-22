e1fa22ed9d7baa7fb441749822210dbf9f8a4f9c
Rodeeo <49972420+Rodeeo@users.noreply.github.com>
2020-10-21 20:26:12 -0600

    todo

--- WDD330/Week-5/TODO/todo.js
+++ WDD330/Week-5/TODO/todo.js
@@ -1,88 +1,55 @@
-var todoList = document.getElementById("todoList");
-var doneList = document.getElementById("doneList");
-
-function add() {
-    var item = document.getElementById("newTodo").value;
-    var itemTxt = document.createTextNode(item);
-    var li = document.createElement("li");
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    var btn = document.createElement("button");
-    var btnx = document.createTextNode("x");
-    btn.setAttribute("onclick", "remove()");
-    btn.appendChild(btnx);
-    checkbox.setAttribute("onclick", "complete()");
-    li.appendChild(checkbox);
-    li.appendChild( document.createTextNode( '\u00A0' ) );
-    li.appendChild(itemTxt);
-    li.appendChild( document.createTextNode( '\u00A0' ) );
-    li.appendChild(btn);
-    todoList.appendChild(li);
-    localStorage["list.a"] = toDoList.innerHTML;
-  }
-
-function complete() {
-  var task = this.event.currentTarget.parentNode;
-  if(task.style.backgroundColor === "lightgreen"){
-    task.style.backgroundColor = null;
-    todoList.appendChild(task);
-    doneList.removeChild(task);
-  }
-  else{
-    task.style.backgroundColor = "lightgreen";
-    todoList.removeChild(task);
-    doneList.appendChild(task);
-  }
-  localStorage["list.a"] = todoList.innerHTML;
-  localStorage["list.b"] = doneList.innerHTML;
+// Create a "close" button and append it to each list item
+var todolist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < todolist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  todolist[i].appendChild(span);
 }
 
-function remove() {
-  var task = this.event.currentTarget.parentNode;
-  event.currentTarget.parentElement.remove();
-  localStorage["list.a"] = todoList.innerHTML;
-  localStorage["list.b"] = doneList.innerHTML;
-}
-
-function showTodo() {
-  var x = document.getElementById("todoList");
-  var y = document.getElementById("doneList");
-
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }
-
-  y.style.display = "none";
-
-}
-
-function showCompletedTodo() {
-  var x = document.getElementById("todoList");
-  var y = document.getElementById("doneList");
-
-  if (y.style.display === "none") {
-    y.style.display = "block";
-  }
-
-  x.style.display = "none";
-}
-
-function showAll() {
-  var x = document.getElementById("todoList");
-  var y = document.getElementById("doneList");
-
-  if (x.style.display === "none") {
-      x.style.display = "block";
-  }
-  if (y.style.display === "none") {
-      y.style.display = "block";
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
   }
 }
 
-if (localStorage["list.a"]) {
-    todoList.innerHTML = localStorage["list"];
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
   }
-  
-  if (localStorage["list.b"]) {
-    doneList.innerHTML = localStorage["list1"];
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("whatTodo").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
   }
+  document.getElementById("whatTodo").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
