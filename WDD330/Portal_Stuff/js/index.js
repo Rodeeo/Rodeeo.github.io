893c2169348aa641441918a72f8ffe8db5510a0e
Rodeeo <49972420+Rodeeo@users.noreply.github.com>
2020-12-11 13:29:17 -0700

    portfolio

--- WDD330/Portal_Stuff/js/index.js
+++ WDD330/Portal_Stuff/js/index.js
@@ -1,5 +1,133 @@
-var lastMod = document.lastModified;
-document.getElementById('lastMod').textContent = lastMod;
+const currentPartyStatus = document.querySelector('.js-status'),
+      bigButton = document.querySelector('.js-big-button'),
+      partyScreen = document.querySelector('.js-party-screen'),
+      ballDrop = document.querySelector('.theparty');
 
-const update = {year:'numeric'};
-document.getElementById('currentYear').textContent = new Date().toLocaleDateString('en-US', update);
+const party = () => {
+  if(bigButton.classList.contains('is-active')) {
+    bigButton.classList.remove('is-active');
+    partyScreen.classList.remove('is-active');
+    currentPartyStatus.innerHTML = "Don't Press It";
+    ballDrop.classList.remove('is-active');
+    bigButton.classList.add('is-active');
+    createLights();{return;}
+
+    
+  } else {
+    ballDrop.classList.add('is-active');
+    bigButton.classList.add('is-active');
+    partyScreen.classList.add('is-active');
+    currentPartyStatus.innerHTML = '🎉 Merry Christmas!🎉';
+    partyTimer()
+  }  
+}
+
+
+bigButton.addEventListener('click', party);
+
+
+let lazerbeams = [];
+let lights = [];
+
+function stopParty() {
+    function startParty(){ return; 
+} }
+
+let colors = ["#ff7979", "#686de0", "#badc58", "#f6e58d", "#ffbe76", "#e056fd", "#7ed6df", "#dff9fb"];
+
+for(let x = 0; x < 8; x++){
+  lazerbeams.push(document.getElementById("beam" + x));
+}
+
+function partyTimer(){
+    timer = setTimeout(startParty, 3000)
+}
+
+function startParty(){
+  document.body.style = "background: black; color: white; !important;";
+  createLights();
+  update();
+ 
+  
+  window.addEventListener("resize", remakeLights);
+}
+
+function update(){
+  let time = Date.now()/2750;
+  let xLoc = window.innerWidth/2;
+  for(let z = 0; z < 8; z++){
+    lazerbeams[z].style = `left: ${xLoc}px; 
+                      top: -5px;
+                      display: block;
+                      transform-origin: top center;
+                      background: white;
+                      box-shadow: 0px 0px 30px 10px ${colors[z]}, 0px 0px 150px 10px ${colors[z]};
+                      transform: rotate(${(time % 360) * 180 + (z*45)}deg)`;
+  }
+  
+  window.requestAnimationFrame(update);
+}
+
+setInterval(shuffleArray, 1000, colors);
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function getLightStyle(x, y, color){
+  return `left: ${x}px; top: ${y}px;
+          box-shadow:
+              0px 0px 10px 10px white,
+              0px 0px 30px 30px ${color},
+              0px 100px 100px 30px ${color};`;
+}
+
+function remakeLights(){
+  for(let z = 0; z < lights.length; z++){
+      lights[z].remove();
+  }
+  createLights();
+}
+
+function createLights(){
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  
+  for(let x = 0; x < width; x+= 150){
+    let randomColor = colors[Math.floor(colors.length * Math.random())];
+    let lightTop = document.createElement("div");
+    lightTop.classList.add("bottom-light");
+    lightTop.style = getLightStyle(x, 0, randomColor);
+    
+    let lightbottom = document.createElement("div");
+    lightbottom.classList.add("bottom-light");
+    lightbottom.style = getLightStyle(x, height-10, randomColor);
+    
+    lights.push(lightTop);
+    lights.push(lightbottom);
+    document.body.append(lightTop);
+    document.body.append(lightbottom);
+  }
+  
+  for(let y = 0; y < height; y += 150){
+    let randomColor = colors[Math.floor(colors.length * Math.random())];
+    let lightLeft = document.createElement("div");
+    lightLeft.classList.add("bottom-light");
+    lightLeft.style = getLightStyle(0, y, randomColor);
+    
+    let lightRight = document.createElement("div");
+    lightRight.classList.add("bottom-light");
+    lightRight.style = getLightStyle(width - 10, y, randomColor);
+    
+    lights.push(lightLeft);
+    lights.push(lightRight);
+    document.body.append(lightLeft);
+    document.body.append(lightRight);
+  }
+}
+
--- WDD330/Portal_Stuff/js/main.js
+++ WDD330/Portal_Stuff/js/main.js
@@ -1,6 +1,72 @@
 const links = [
     {
-      label: "Week1 notes",
-      url: "/WDD 330/Week 1/index.html"
+      label: "Week 2 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-2/notes2.html"
+    },
+    {
+      label: "Week 3 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-3/notes3.html"
+    },
+    {
+      label: "Week 4 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-4/notes4.html"
+    },
+    {
+      label: "Week 5 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-5/notes5.html"
+    },
+    {
+      label: "Week 6 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-6/notes6.html"
+    },
+    {
+      label: "Week 7 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-7/notes7.html"
+    },
+    {
+      label: "Week 8 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-8/notes8.html"
+    },
+    {
+      label: "Week 9 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-9/notes9.html"
+    },
+    {
+      label: "Week 10 notes",
+      url: "https://rodeeo.github.io/WDD330/Week-10/notes10.html"
+    },
+    {
+      label: "Challenge 1 - To Do",
+      url: "https://rodeeo.github.io/WDD330/Week-6/Todo/todo.html"
+    },
+    {
+      label: "Challenge 2 - Beatbox",
+      url: "https://rodeeo.github.io/WDD330/beatbox/index.html"
     }
   ]
+  links.forEach(element => {
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.setAttribute('href', element.url);
+    ul.appendChild(li).appendChild(a).innerHTML = element.label;
+  });
+
+  const beams = [
+    { id:"beam0", class:"beam"},
+    { id:"beam1", class:"beam"},
+    { id:"beam2", class:"beam"},
+    { id:"beam3", class:"beam"},
+    { id:"beam4", class:"beam"},
+    { id:"beam5", class:"beam"},
+    { id:"beam6", class:"beam"},
+    { id:"beam7", class:"beam"}
+    ]
+    
+beams.forEach(element => {
+  let laser = document.querySelector('.nada');
+  let beam = document.createElement("div");
+  beam.setAttribute("id", element.id);
+  beam.setAttribute("class", element.class);
+  laser.appendChild(beam)
+});
